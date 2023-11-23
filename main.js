35057f79f591663216f5f076cc83f7d956f226b9
lianqq3 <115630478+lianqq3@users.noreply.github.com>
2023-11-23 14:17:25 +0800

    added a button

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
-function createWindow(){
-    const win = new BrowserWindow({
+let mainWindow; // Declare a variable to store the reference to the main window
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
         width: 768,
         height: 560,
     });
 
-    win.loadFile('src/index.html')
+    mainWindow.loadFile('src/index.html');
+
+    // Send a message to the renderer process after the window is ready
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.webContents.send('window-loaded');
+    });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') app.quit;
-}) // Hello
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// Handle the 'button-click' event sent from the renderer process
+ipcMain.on('button-click', (event, name) => {
+ 
+    alert(`Hello ${name}, nice to see you!`);
+    mainWindow.webContents.send('update-heading', name);
+});
--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
 console.log("whats up mga burat")
+
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on('window-loaded', () => {
+    const buttonA = document.getElementById('Button_A');
+    const headingA = document.getElementById('heading_A');
+
+    buttonA.addEventListener('click', () =>{
+        const name = prompt("What is your name?");
+        alert(`Hello Name ${name}, its nice to see you!`);
+        headingA.textContent = `Welcome ${name}`;
+    });
+});
+
